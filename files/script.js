8c65f23b1fb4205b6b3f6df2aaa5967af349a584
mohit2713 <mohitkashyap0813@gmail.com>
2022-04-23 13:09:44 +0530

    first commit

---
+++ files/script.js
@@ -0,0 +1,25 @@
+
+var form = document.getElementById("myForm");
+function handleForm(event) { event.preventDefault(); } 
+form.addEventListener('submit', handleForm);
+
+function validateform(e){ 
+ 
+let name = document.forms["myForm"]["fname"].value;
+console.log(name)
+
+  
+if (name==null || name==""){  
+  alert("Name can't be blank");  
+  return false;  
+}  
+  
+else if (name.length > 20){  
+    alert("Name is too long");  
+    return false;  
+  }
+else if(/\d/.test(name)){
+    alert("Name should not contain number");  
+    return false; 
+}
+}  
